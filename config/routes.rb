86acbc3d350088572c1f719f777e466f320da60d
jsleetw <ail529@gmail.com>
2012-03-23 16:44:17 +0800

    finish micropost model adding model input interface

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ SampleApp::Application.routes.draw do
 
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
+  resources :microposts, only: [:create, :destory]
 
   match '/signup',  to: 'users#new'
   match '/signin',  to: 'sessions#new'
